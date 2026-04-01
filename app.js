713d4628eee45a8737e139c4c57585ab869534ca
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-01 12:01:54 +0200

    add express dependencs

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//ENTRY POINT APP
+app.get('/', function(req, res) {
+    res.send('PAGINA PRINCIPALE')
+})
+
+
+//Avvio il mio server e lo metto in ascolto alla mia porta
+app.listen(port, () => {
+    console.log(`Il server è attivo ed è in ascolto alla porta ${port}`)
+})
